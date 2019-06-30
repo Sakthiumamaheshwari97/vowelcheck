a0d170184144563f588d268ed1bdab4b895ea6e1
Sakthiumamaheshwari97 <51943276+Sakthiumamaheshwari97@users.noreply.github.com>
2019-06-30 09:46:15 -0700

    Update vowel.py

--- vowel.py
+++ vowel.py
@@ -1,6 +1,6 @@
 fi=input()
 for i in fi:
-   if(i=="a" or i=="e" or i="i" or i="o" or i="u" or i="A" or i="E" or i="I" or i="O" or i="U"):
+   if(i=="a" or i=="e" or i=="i" or i=="o" or i=="u" or i=="A" or i=="E" or i=="I" or i=="O" or i=="U"):
         print("yes")
         break
 else:
